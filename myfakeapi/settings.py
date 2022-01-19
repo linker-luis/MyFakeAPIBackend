1c414af91e3b5dc30603d743704d31bd1e67310c
linker-luis <luis.dot444@gmail.com>
2022-01-19 18:29:17 -0500

    psycopg2

--- myfakeapi/settings.py
+++ myfakeapi/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'myfakeapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': ‘<database_name>’,
-#        'USER': '<database_username>',
-#        'PASSWORD': '<password>',
-#        'HOST': '<database_hostname_or_ip>',
-#        'PORT': '<database_port>',
-#    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'dfshatck2f1vhc',
+       'USER': 'borbziwezgorqo',
+       'PASSWORD': '57a36d4a0bfc90b4dd498489e847cba336da499e77d68f37fd889489c97e9b5e',
+       'HOST': 'ec2-3-222-24-200.compute-1.amazonaws.com',
+       'PORT': '5432',
+   }
+}
 
 
 # Password validation
