541fca1108d31d27fafaf2baa37a7ce71f9398dc
linker-luis <luis.dot444@gmail.com>
2022-03-31 00:27:53 -0500

    'production'

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfakeapi.settings.local')
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfakeapi.settings.production')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfakeapi.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfakeapi.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
